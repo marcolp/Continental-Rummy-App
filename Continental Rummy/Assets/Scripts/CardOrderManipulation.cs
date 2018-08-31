dcbfc3b84de8edf32c1cb2bc4782d3273e7834c1
alexBHVR <alex@beaconhillvr.com>
2018-08-30 19:58:45 -0600

    Added CardOderManipulation
    Created CardOrderManipulation. Class used to compare, sort, shuffle (randomize) decks, etc.

---
+++ Continental Rummy/Assets/Scripts/CardOrderManipulation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+static class CardOrderManipulation {
+
+
+    public static List<Card> sortCards(List<Card> cardSet)
+    {
+        List<Card> setCopy = new List<Card>(cardSet); //MIGHT BE SAME REFERENCE. DONT WANT THAT
+
+
+
+        return setCopy;
+    }
+
+    /**
+     * Method to compare the value of two cards.
+     * if card1 is greater than card2 then it returns 1,
+     * if card1 is equal to card2 then it returns 0,
+     * if card1 is less than card2 then it returns -1.
+     * if card1 or card2 is an ACE, then the ace is always larger
+     * UNLESS we are trying to make a run with ACE, two, etc.
+     */ 
+    public static int compareCardsV(Card card1, Card card2)
+    {
+        int card1Value = (int)card1.getValue();
+        int card2Value = (int)card2.getValue();
+        int result = -2; 
+
+        //If the cards being compared are aces, then have them be the highest value
+        if(card1.getValue() == Value.ACE) card1Value = 14;
+        if(card2.getValue() == Value.ACE) card2Value = 14;
+        
+
+
+        if (card1Value > card2Value) result = 1;
+
+        else if (card1Value == card2Value) result = 0;
+
+        else result = -1;
+        return result;
+    }
+
+}
--- Continental Rummy/Assets/Scripts/HandValueCheck.cs
+++ Continental Rummy/Assets/Scripts/HandValueCheck.cs
@@ -17,17 +17,78 @@ public class HandValueCheck : MonoBehaviour {
 		
 	}
 
+    /**
+     * Method to check if the set of cards is a run.
+     * A run is at least four cards of the same suit
+     * whose value is in ascending order. A run is 
+     * made with 4 of more cards.
+     */
     public bool isRun(List<Card> cardSet)
     {
+        if (cardSet.Count < 4) return false; //Not enough cards to be a run
+
+        if (!areSameSuit(cardSet)) return false; //Cards have different suits
+
+
+
         return false;
     }
 
+    /**
+     * Method to check if the set of cards are the same suit.
+     */
+    public bool areSameSuit(List<Card> cardSet)
+    {
+        Suit tempSuit = 0;
+        int counter = 0;
+
+        //Ceck that all cards have the same suit
+        foreach (Card currentCard in cardSet)
+        {
+            //Take the first card and compare it to the rest
+            if (counter == 0)
+            {
+                tempSuit = currentCard.getSuit();
+                continue;
+            }
+
+            else
+            {
+                
+                if (tempSuit == currentCard.getSuit())
+                {
+                    continue;
+                }
+
+                //If any of the cards is a different suit then it is not a trio
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        //If we compared all the cards that means they are the same!
+        return true;
+    }
+
+    /**
+     * Method to check if a set of cards is a trio.
+     * A trio is made with at least 3 cards.
+     * NOTE that despite it being called a trio, there could
+     * be more than three. As long as all have the same value
+     * it is considered a trio
+     */
     public bool isTrio(List<Card> cardSet)
     {
+        if (cardSet.Count < 3) return false; //Not enough cards to be a tio
+
         Value tempValue = 0;
         int counter = 0;
+
         foreach(Card currentCard in cardSet)
         {
+            //Take the first card and compare it to the rest
             if (counter == 0)
             {
                 tempValue = currentCard.getValue();
@@ -36,17 +97,21 @@ public class HandValueCheck : MonoBehaviour {
 
             else
             {
+               
                 if(tempValue == currentCard.getValue())
                 {
-                    return true;
+                    continue;
                 }
 
+                //If any of the cards is a different value then it is not a trio
                 else
                 {
                     return false;
                 }
             }
         }
-        return false;
+
+        //If we compared all the cards that means they are the same!
+        return true;
     }
 }
